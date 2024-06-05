533d045f91af0f2e8549fc5176104888a1c8a7a8
Esmeralda Guadalupe Morales León <84791288+211107@users.noreply.github.com>
2024-06-05 16:22:38 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require ('express');
 const app = express();
-const port = 4000;
+const port = 3000;
 
 app.get('/api/v2/welcome', (req, res) => {
     res.send('Hola como estas!!');
