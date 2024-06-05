e507b28a8c93f79435b00865a960b106e23a555b
211107 <211107@ids.upchiapas.edu.mx>
2024-06-04 20:42:37 -0600

    se cambio el mensaje

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require ('express');
 const app = express();
 const port = 3000;
 
-app.get('/api/v1/welcome', (req, res) => {
-    res.send('Hello, World!!');
+app.get('/api/v2/welcome', (req, res) => {
+    res.send('Hola como estas!!');
 });
 
 app.listen(port, () => {
