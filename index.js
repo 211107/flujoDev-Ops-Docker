cbc507e805372c8a608ed0e0ac980afb0131e857
Esmeralda Guadalupe Morales León <84791288+211107@users.noreply.github.com>
2024-06-07 01:21:25 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get('/api/v2/welcome', (req, res) => {
-    res.send('Hola !!');
+    res.send('Hola!!!!!!!!!!!!!');
 });
-
+ 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
